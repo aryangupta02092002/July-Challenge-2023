0257195f6485dc617a6b13c0515976e9c4b1727a
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-07-15 14:02:31 +0530

    July 15 - Stack

---
+++ GFG/DeleteMiddleElementOfAStack.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+    public:
+    //Function to delete middle element of a stack.
+    void deleteMid(stack<int>&s, int sizeOfStack)
+    {
+        // code here.. 
+        stack<int> st;
+        for(int i=sizeOfStack; i>ceil((sizeOfStack+1)/2); i--){
+            int curr = s.top();
+            s.pop();
+            st.push(curr);
+        }
+        s.pop();
+        for(int i=st.size(); i>0; i--){
+            int curr = st.top();
+            st.pop();
+            s.push(curr);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+        int sizeOfStack;
+        cin>>sizeOfStack;
+        
+        stack<int> myStack;
+        
+        for(int i=0;i<sizeOfStack;i++)
+        {
+            int x;
+            cin>>x;
+            myStack.push(x);    
+        }
+
+            Solution ob;
+            ob.deleteMid(myStack,myStack.size());
+            while(!myStack.empty())
+            {
+                cout<<myStack.top()<<" ";
+                myStack.pop();
+            }
+        cout<<endl;
+    }   
+    return 0;
+}
+
+// } Driver Code Ends
